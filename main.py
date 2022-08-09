a80caa62ea3e615722cf23b440e3bd1002278c73
Jose Vilson <contatojosevilson@gmail.com>
2022-08-09 00:13:25 -0300

    nada

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-Print ('testando github')
+from msilib.schema import Class
+
+Print('testando github')
+
+Class Agencia:
+    def __init__(self, testando):
+        self.testando = 1
+
+    def Imprimir(self, Valor):
+        self.Valor = Valor
+        return print(Valor)
